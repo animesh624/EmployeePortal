388309481957733dd7e02e130b5f99e9bcbdcc3b
animesh.dubey <animesh.dubey@moneyview.in>
2024-02-27 14:00:37 +0530

    added employee api

--- src/main/java/com/example/employeeportal/controller/EmployeeController.java
+++ src/main/java/com/example/employeeportal/controller/EmployeeController.java
@@ -1,4 +1,22 @@
 package com.example.employeeportal.controller;
 
+
+import com.example.employeeportal.model.EmployeeData;
+import com.example.employeeportal.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @PostMapping
+    public EmployeeData getByUserName(@RequestParam String userName) throws Exception{
+        return employeeService.getByUserName(userName);
+    }
 }
--- src/main/java/com/example/employeeportal/services/EmployeeService.java
+++ src/main/java/com/example/employeeportal/services/EmployeeService.java
@@ -1,7 +1,10 @@
 package com.example.employeeportal.services;
 
 import com.example.employeeportal.model.EmployeeData;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface EmployeeService {
 
     public EmployeeData getByUserName(String userName) throws Exception;
