8d7534a607388c186cc3db62a833dc7edcd9f266
animesh.dubey <animesh.dubey@moneyview.in>
2024-02-29 12:37:18 +0530

    unwanted import removed

--- src/main/java/com/example/employeeportal/controller/EmployeeController.java
+++ src/main/java/com/example/employeeportal/controller/EmployeeController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class EmployeeController {
 
