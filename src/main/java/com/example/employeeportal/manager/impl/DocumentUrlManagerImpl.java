1734fc85cb09d9d67a2730f278822718356386f0
animesh.dubey <animesh.dubey@moneyview.in>
2024-03-12 12:26:16 +0530

    resolved query of getUserEmail not giving response

--- src/main/java/com/example/employeeportal/manager/impl/DocumentUrlManagerImpl.java
+++ src/main/java/com/example/employeeportal/manager/impl/DocumentUrlManagerImpl.java
@@ -3,12 +3,14 @@ package com.example.employeeportal.manager.impl;
 import com.example.employeeportal.manager.DocumentUrlManager;
 import com.example.employeeportal.model.DocumentUrl;
 import com.example.employeeportal.repo.DocumentUrlRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Slf4j
 public class DocumentUrlManagerImpl extends GenericManagerImpl<DocumentUrl,String> implements DocumentUrlManager {
 
     DocumentUrlRepo documentUrlRepo;
