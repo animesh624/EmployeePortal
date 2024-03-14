a22bffc434a8ecd1b0812459495e679a1a072a0f
animesh.dubey <animesh.dubey@moneyview.in>
2024-03-14 22:37:44 +0530

    to trigger job

--- src/main/java/com/example/employeeportal/controller/EmployeeController.java
+++ src/main/java/com/example/employeeportal/controller/EmployeeController.java
@@ -65,7 +65,6 @@ public class EmployeeController {
         return employeeService.searchEmployee(name, designation, userEmail, skill, language, interest);
     }
 
-
     // TODO seperate folder for pdf and images.
     @PostMapping(path = "/uploadDocument")
     public ResponseEntity<Object> uploadDocument(@RequestParam("file") MultipartFile file,
